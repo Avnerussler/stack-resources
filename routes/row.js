1c5890ec429083b75d8dbf7d7025dea799f9cfe1
Avner Russler <52633686+Avnerussler@users.noreply.github.com>
2020-06-14 16:18:04 +0300

    Update row.js

--- routes/row.js
+++ routes/row.js
@@ -8,3 +8,35 @@ router.route('/').get((req,res)=>{
     .then(row=>res.json(row))
     .catch(err=>res.status(400).json('Error'+err))
 })
+
+router.route("/add").post((req,res)=>{
+    const department=req.body.department;
+    const numOfStacks= Number(req.body.numOfStacks)
+    const usage= Number(req.body.usage)
+    const owners=  req.body.owners;
+    const stackType= req.body.stackType;
+    const RAM=                 Number(req.body.RAM)
+    const notAssigned= Number( req.body.notAssigned)
+    const allocationCapacity= Number(req.body.allocationCapacity)
+    const physicalUsage=Number( req.body.physicalUsage)
+    const CPU=Number(req.body.CPU)
+
+    const newRow=new Row({
+        department,
+     numOfStacks,
+     usage,
+     owners,
+     stackType,
+     RAM,
+     notAssigned,
+     allocationCapacity,
+     physicalUsage,
+     CPU,
+
+    })
+    newRow.save()
+    .then(()=>res.json("row add"))
+    .catch(err=>res.status(400).json("Error:"+err))
+})
+
+module.exports=router
